7e79720163082db5125ba12a913572f21a7ba6ef
Marcos Leite <m.abner@live.co.uk>
2022-08-15 01:33:33 -0300

    Adjust news menus

--- app/src/main/java/com/example/footballnews/ui/news/NewsViewModel.java
+++ app/src/main/java/com/example/footballnews/ui/news/NewsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.footballnews.ui.home;
+package com.example.footballnews.ui.news;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class NewsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public NewsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is news fragment");
     }
 
     public LiveData<String> getText() {
