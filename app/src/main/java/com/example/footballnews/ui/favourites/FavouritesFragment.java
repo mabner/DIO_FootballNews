232ed52fd5a4a6680a284d8d182027d1d5997c87
Marcos Leite <m.abner@live.co.uk>
2022-08-15 01:15:28 -0300

    Adjust favourite menus

---
+++ app/src/main/java/com/example/footballnews/ui/favourites/FavouritesFragment.java
@@ -0,0 +1,35 @@
+package com.example.footballnews.ui.favourites;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.footballnews.databinding.FragmentFavouritesBinding;
+
+public class FavouritesFragment extends Fragment {
+
+    private FragmentFavouritesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        FavouritesViewModel favouritesViewModel =
+                new ViewModelProvider(this).get(FavouritesViewModel.class);
+
+        binding = FragmentFavouritesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        favouritesViewModel.getText().observe(getViewLifecycleOwner(), binding.textFavourites::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
